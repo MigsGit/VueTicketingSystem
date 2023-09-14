c33a307ffadf6a5d7493cd04188d24068560badb
MigsGit <miguellgsp12345@gmail.com>
2023-09-14 16:15:07 +0800

    Resolving Merge Error

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LaraVue Ticketing System</title>
-    @vite(['/resources/sass/app.scss','/resources/js/app.js'])
+    @vite(['/resources/sass/app.scss','resources/js/app.js'])
 </head>
 <body class="font-sans antialiased sb-nav-fixed">
     <div  id="app">
